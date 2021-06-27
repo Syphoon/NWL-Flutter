0a231f86d05a64a671348d60b8709d8f49f15706
Syphoon <smvasconcelos11@gmail.com>
2021-06-26 21:33:40 -0300

    Master Completo

--- README.md
+++ README.md
@@ -1,16 +1,62 @@
 <h1 align="center"> PayFlow </h1>
 
+<div align="center">
+
+<img alt="Gif da Aplicação" src=".github\app.gif"  height="430">
+
+</div>
 
 <p align="center">
 <img alt="flutter version badge" src=".github\flutter.svg">
+<img alt="dart version badge" src=".github\dart.svg">
 <img alt="rocketseat badge" src=".github\rocket.svg">
-<img alt="nwl day_01" src=".github\nwl_1.svg">
+<img alt="nwl day_04" src=".github\nwl.svg">
 </p>
 
-## Sobre
+<h1 align="center">Desafios</h1>
 
-Aplicativo está sendo desenvolvido durante a [NLW](https://nextlevelweek.com/) na trilha Flutter. A proposta inicial é um aplicativo para gerenciamento de boletos e manter um controle de estados de pagamento, com possibilidade de visualização de histórico de cadastro e estados.
 
-## Desenvolvimento
+- [x] Filtro Mensal
+- [x] Contagem de Boletos
+- [ ] Logout
+- [ ] Bottom Modal
 
-Esta é minha primeira experiência com flutter, não tenho familiaridade com a linguagem dart ou seus elementos. Com a primeira aula da NWL deu pra entender e captar as similaridades da linguagem dart com outras ferramentas que trazem propostas semelhantes (React, React Native , etc ...).
+<h1 align="center"> Sobre </h1>
+
+<p>
+	Aplicativo foi desenvolvido durante a NLW na trilha Flutter. O PayFlow se trata de um app mobile feito em Flutter  para Gerenciamento de Boletos, contendo recursos como o uso de câmera e galeria, Machine Leaning com MLKit, Firebase Core e SignIn, Animações e Estilizações Personalizadas, entre vários outros pontos, como o uso do SharedPreferences.
+</p>
+
+
+<h1 align="center"> Desenvolvimento </h1>
+
+Esta é minha primeira experiência com flutter, não tenho familiaridade com a linguagem dart ou seus elementos. Com a primeira aula da NWL deu pra entender e captar as similaridades da linguagem dart com outras ferramentas que trazem propostas semelhantes (React, React Native , etc ...). A partir dessas familiaridades foi possível construir uma base bem legal pois ficou mais fácil de assimilar os elementos e o que estava acontecendo pouco a pouco em cada aula. Foi uma experiência muito boa e lucrativa para a aprendizado e acredito que mais pessoas deviam participar deste evento, os intrutores e a galera da comunidade estão sempre disponível no discord, juntos, desenvolvendo nessa semana cooperativa, para que no fim todos alcacemos o próximo nível.
+
+<h1 align="center"> Mockup </h1>
+---
+<p align="center">
+      <img alt="PayFlow" title="PayFlow" src=".github/capa.png" />
+   </p>
+
+   <p align="center">
+      O Layout foi desenvolvido por <a href="https://instagram.com/tiagoluchtenberg">Tiago Luchtenberg</a>, e você pode acessá-lo no Figma: <a href="https://www.figma.com/file/kLK7FYnWKMoN68sQXcSniu/PayFlow">Mockup</a>
+</p>
+
+
+<h1 align="center"> Como Usar </h1>
+
+```
+- Clone esse repositório:
+$ git clone https://github.com/Syphoon/NWL-Flutter
+
+- Entre no diretório:
+$ cd NWL-Flutter
+
+- Instale as dependências:
+$ flutter pub get
+
+- Inicie o app:
+$ flutter run
+```
+
+---
---
+++ lib/modules/extract/extract_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
+
+class ExtractPage extends StatefulWidget {
+  const ExtractPage({Key? key}) : super(key: key);
+
+  @override
+  _ExtractPageState createState() => _ExtractPageState();
+}
+
+class _ExtractPageState extends State<ExtractPage> {
+  final controller = BoletoListController();
+  String dropdownValue = 'Todos';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Meus extratos",
+                  style: TextStyles.titleBoldHeading,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.stroke)
+                  ),
+                  width: 150,
+                  height: 55,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward, color: Colors.orange,),
+                      iconSize: 24,
+                      isExpanded: true,
+                      elevation: 16,
+                      style: TextStyles.buttonBoldHeading,
+                      underline: Container(
+                        height: 2,
+                        color: Colors.transparent,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>[
+                        'Todos',
+                        'Janeiro',
+                        'Fevereiro',
+                        'Março',
+                        'Abril',
+                        'Maio',
+                        'Junho',
+                        'Julho',
+                        'Agosto',
+                        'Setembro',
+                        'Outubro',
+                        'Novembro',
+                        'Dezembro',
+                      ].map<DropdownMenuItem<String>>((value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+            child: Divider(
+              thickness: 1,
+              height: 1,
+              color: AppColors.stroke,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: BoletoListWidget(
+              controller: controller,
+              monthFilter: dropdownValue,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
